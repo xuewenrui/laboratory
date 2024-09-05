b645588b85016359f96e002a993b73957e3cade4
袁瑞 <2502099390@qq.com>
2024-09-05 11:47:10 +0800

    第十五次提交

--- config/config.ts
+++ config/config.ts
@@ -6,16 +6,18 @@ export default defineConfig({
      *  代理
      */
 
-        proxy: {
-            '/api/': {
-                target: 'http://127.0.0.1:5000/api', // 后端服务地址
-                changeOrigin: true, // 是否跨域
-                pathRewrite: { '^/api': '' }, // 路径重写
-            },
+    proxy: {
+        '/api/': {
+            target: 'http://127.0.0.1:5000/api', // 后端服务地址
+            changeOrigin: true, // 是否跨域
+            pathRewrite: {'^/api': ''}, // 路径重写
         },
-
+    },
+    history: {
+        type: 'hash',
+    },
     routes,
     npmClient: 'npm',
-    base:'/laboratory/',
+    base: '/laboratory/',
     publicPath: '/laboratory/', // 替换为你的 GitHub 仓库名
 });
